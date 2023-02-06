98fa054033b9d540338bda3bf3e3f68d6e7aac7a
Leonid <enykoruna1@gmail.com>
2023-02-06 11:06:33 +0200

    Update ViewComponent

---
+++ TvShows/Components/StatisticsViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TvShows.Data;
+using TvShows.ViewModels;
+
+namespace TvShows.Components
+{
+    public class StatisticsViewComponent : ViewComponent
+    {
+        private readonly ApplicationContext _context;
+
+        public StatisticsViewComponent(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var genreCount = await _context.TvShow.GroupBy(e => e.Genre).Select(e => new
+            {
+                Key = e.Key,
+                Count = e.Count()
+            }).ToDictionaryAsync(k => k.Key, c => c.Count);
+
+            return View(new StatisticsViewModel
+            {
+                GenreCount = genreCount,
+                TotalCount = genreCount.Values.Sum()
+            });
+        }
+    }
+}
---
+++ TvShows/ViewModels/StatisticsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using TvShows.Models;
+
+namespace TvShows.ViewModels
+{
+    public class StatisticsViewModel
+    {
+        public int TotalCount { get; set; }
+        public Dictionary<Genre, int> GenreCount { get; set; }
+    }
+}
